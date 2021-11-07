7df4ea1be5cae110c94332c6dccf86300e9ad146
Thales Voltz <voltz.thales@gmail.com>
2021-11-07 00:24:21 -0300

    Meu Primeiro Commit

---
+++ app/src/main/java/com/example/applayout/AgendaActivity.kt
@@ -0,0 +1,11 @@
+package com.example.applayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AgendaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agenda)
+    }
+}
